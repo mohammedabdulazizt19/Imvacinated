9374eafcf6fc807c5fdb94944df9e679514f9be3
Mohammed <Aziz.Mohammed@Aziz-Mohammed.local>
2022-02-04 02:36:16 +0800

    hadji:update in about us page

--- resources/views/livewire/myaboutus.blade.php
+++ resources/views/livewire/myaboutus.blade.php
@@ -27,10 +27,9 @@
                     </div>
                 </div>
             </div>
-            <div class="row align-items-center section-overlay">
+            <div class="row align-items-center section-overlay mb-30">
                 <div class="offset-xl-1 col-xxl-5 col-xl-6 col-lg-6 col-md-9">
                     <div class="about-caption about-caption1">
-
                         <div class="section-tittle section-tittle3 mb-25">
                             <h2>Call on MY KABARKADA</h2>
                             <p>To the whole Family ,, to the whole barangay ,,, There will be an office at work ,,, to the lovers who still see each other night after night joke ... 😝</p>
@@ -38,15 +37,34 @@
                             <p>Maghihiway din kayo !!!</p>
                             <p>joke lang 😝 😂 😜</p>
                             <p>this is what suits the whole group and the whole family the Boodle Fight of the islas  resto and grill 👊 👊 👊 💪 💪 </p>
-                            <p>also for those who don't know that other Filipino foods are also available... 
-All other Filipino foods such as KARE-KARE, ZIZLING VARIETY ,,, BREAKFAST like TAPSILOG ,, DAINGSILOG ,, LONGSILOG, TOCILOG ,, PANCIT BIHON ,, LUMPIANG SHANGHAI AND LUMPIANG SARIWA are also available which are popular and can also be sold at ISLAS RESTO AND GRILL..</p>
+                            <p>also for those who don't know that other Filipino foods are also available... All other Filipino foods such as KARE-KARE, ZIZLING VARIETY ,,, BREAKFAST like TAPSILOG ,, DAINGSILOG ,, LONGSILOG, TOCILOG ,, PANCIT BIHON ,, LUMPIANG SHANGHAI AND LUMPIANG SARIWA are also available which are popular and can also be sold at ISLAS RESTO AND GRILL..</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xxl-5 col-xl-5 col-lg-6 col-md-8">
+                    <div class="about-img about-img1">
+                        <img src="{{ asset('themes/cakeshop2/assets/img/boodles/boodles_2.jpeg')}}" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="row align-items-center section-overlay mb-30">
+                <div class="offset-xl-1 col-xxl-5 col-xl-6 col-lg-6 col-md-9">
+                    <div class="about-caption about-caption1">
+                        <div class="section-tittle section-tittle3 mb-25">
+                            <h2>Boodle fight round #2 is up!</h2>
+                            <p>Call now and reserve your seat.</p>
+                            <p>Tel# 0503 881 180</p>
+                            <p>Sulaimaniyah area, Riyadh, Saudi Arabia</p>
+                            <p>Thursdays : Sound Fuzion Band (KTV)</p>
+                            <p>Fridays : Live Band</p>
+                            <p>Saturdays : Acoustic Sessions/ Live Band 6:00 PM to 10:30 PM</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-xxl-5 col-xl-5 col-lg-6 col-md-8">
 
                     <div class="about-img about-img1">
-                        <img src="{{ asset('themes/cakeshop2/assets/img/boodles/boodles_2.jpeg')}}" alt="">
+                        <img src="{{ asset('themes/cakeshop2/assets/img/boodles/boodles_3.jpeg')}}" alt="">
                     </div>
                 </div>
             </div>
@@ -142,30 +160,30 @@ All other Filipino foods such as KARE-KARE, ZIZLING VARIETY ,,, BREAKFAST like T
                 <div class="col-xl-6 col-lg-7 col-md-7 ">
 
                     <div class="section-tittle  text-center mb-70">
-                        <h2>Follow us on Instagram</h2>
-                        <a href="https://www.facebook.com/ISLAS-Resto-Grill-628961627446509/?ref=page_internal" class="btn_02 btn_02s mt-25"><i class="fab fa-facebook-f"></i>Islas Resto & Grill</a>
+                        <h2>Follow us on Facebook</h2>
+                        <a href="https://www.facebook.com/ISLAS-Resto-Grill-628961627446509/?ref=page_internal" target="_blank" class="btn_02 btn_02s mt-25"><i class="fab fa-facebook-f"></i>Islas Resto & Grill</a>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="instra-img mb-30">
-                        <a href="#"><img src="{{ asset('themes/cakeshop2/assets/img/gallery/xinstra1.jpg.pagespeed.ic.hfROuiYkhu.jpg')}}" alt="" class="w-100"></a>
+                        <a href="#"><img src="{{ asset('themes/cakeshop2/assets/img/foods/food_11.jpeg')}}" alt="" class="w-100"></a>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="instra-img mb-30">
-                        <a href="#"><img src="{{ asset('themes/cakeshop2/assets/img/gallery/xinstra2.jpg.pagespeed.ic.k-mktG-v8O.jpg')}}" alt="" class="w-100"></a>      
+                        <a href="#"><img src="{{ asset('themes/cakeshop2/assets/img/foods/food_8.jpeg')}}" alt="" class="w-100"></a>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="instra-img mb-30">
-                        <a href="#"><img src="{{ asset('themes/cakeshop2/assets/img/gallery/xinstra3.jpg.pagespeed.ic.gbWQpf0S_J.jpg')}}" alt="" class="w-100"></a>
+                        <a href="#"><img src="{{ asset('themes/cakeshop2/assets/img/foods/food_9.jpeg')}}" alt="" class="w-100"></a>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="instra-img mb-30">
-                        <a href="#"><img src="{{ asset('themes/cakeshop2/assets/img/gallery/xinstra4.jpg.pagespeed.ic.t4DoF79Cew.jpg') }}" alt="" class="w-100"></a>
+                        <a href="#"><img src="{{ asset('themes/cakeshop2/assets/img/foods/food_10.jpeg')}}" alt="" class="w-100"></a>
                     </div>
                 </div>
             </div>
